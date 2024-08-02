53180b11f88eaa81e151bd992a8223bebcc5cbeb
ManurangaJay <manurangajayawardhana@gmail.com>
2024-08-02 19:23:32 +0530

    services_data file was added with some dummy data and services component was created

---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web design",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"02",
+        s_name:"Web application design",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"03",
+        s_name:"Machine Learning",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"04",
+        s_name:"Deep Learning",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"05",
+        s_name:"Social Media",
+        s_desc:"Web development is the process of building, programming..."
+    },
+    {
+        s_no:"06",
+        s_name:"Content writing",
+        s_desc:"Web development is the process of building, programming..."
+    },
+]
+ 
+export default Services_Data;
