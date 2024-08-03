f4a2b14930d202cb47d8f80c69146060f0385e37
ManurangaJay <manurangajayawardhana@gmail.com>
2024-08-03 10:54:26 +0530

    The component was created for Contact and add the HTML structure was added

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Contact.css";
+import call_icon from "../../assets/call_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <h1>Get in touch</h1>
+        <hr className="contact-title-hr" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's talk</h1>
+          <p>
+            I'm currently available to work on new projects, So feel free to
+            contact me for your works........
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>manurangajayawardhana@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>+94 70863051 (Whatsapp)</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>770,Sampathgama,Sevanagala</p>
+            </div>
+          </div>
+        </div>
+        <form className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your name" name="name" />
+          <label htmlFor="">Your Email</label>
+          <input type="text" placeholder="Enter your Email" name="email" />
+          <label htmlFor="">Write your message here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
